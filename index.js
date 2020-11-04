a0478cf38b1659288691c8b197321f221c1b0749
yang <y2gcoder@gmail.com>
2020-11-04 10:03:52 +0900

    ~#7 BodyParser & Insomnia & 회원가입 기능

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+//body-parser 가 클라이언트에서 오는 정보를 서버에서 처리할 수 있게 해주는 것. 
+//application/x-www-form-urlencoded 정보 가져오기.
+app.use(bodyParser.urlencoded({extended: true}))
+//application/json 타입을 분석해서 가져오기.
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://yang:admin@boilerplate.l0wik.mongodb.net/test?retryWrites=true&w=majority', {
@@ -9,5 +17,21 @@ mongoose.connect('mongodb+srv://yang:admin@boilerplate.l0wik.mongodb.net/test?re
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/register', (req, res) => {
+    //회원 가입할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터베이스에 넣어준다.
+    //req.body는 body-parser가 있어서 가능함. 
+    const user = new User(req.body) //req.body 안에는 json 형식
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
