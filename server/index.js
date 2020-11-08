5cb63ff7b87f704333aa7d57df9db8b9dc081d6d
yang <y2gcoder@gmail.com>
2020-11-09 08:06:22 +0900

    #21 데이터 Flow & Axios

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/api/hello', (req, res) => {
+    res.send('안녕하세요 ~')
+})
+
 app.post('/api/users/register', (req, res) => {
     //회원 가입할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다.
