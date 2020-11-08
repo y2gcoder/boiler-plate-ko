4880a5ef01bc29078714729ad49b362eed48c6f7
yang <y2gcoder@gmail.com>
2020-11-09 08:21:52 +0900

    #22 CORS 이슈, Proxy 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 function LandingPage() {
     useEffect(() => {
-        axios.get('http://localhost:5000/api/hello')
-        .then(response => console.log(response.data))
+        axios.get('/api/hello')
+        .then(response => console.log(response))
     }, [])
     return (
         <div>
