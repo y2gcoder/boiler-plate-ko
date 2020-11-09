a2c7986da1268146ff0caa6c8b9dc0d8cf0ae0f9
yang <y2gcoder@gmail.com>
2020-11-10 08:20:19 +0900

    #27 Redux UP !

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css'
-
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk'
+import Reducer from './_reducers'
+//원래는 createStore만 하지만 promise와 function도 같이 받게 하기 위해!
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+//react-redux를 이용해 provider로 감싸줌.
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(Reducer, 
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()  
+    )}
+  >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
