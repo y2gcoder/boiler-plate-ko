f29d153a28ae4572bdb3c463e69696a40e7a2a5e
yang <y2gcoder@gmail.com>
2020-11-10 08:42:00 +0900

    #29 로그인 페이지(1)

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,8 +6,10 @@ function LandingPage() {
         .then(response => console.log(response))
     }, [])
     return (
-        <div>
-            LandingPage 랜딩페이지
+        <div style={{ display:'flex', justifyContent:'center', alignItems:'center'
+            , width: '100%', height:'100vh'
+        }}>
+            <h2>시작 페이지</h2>
         </div>
     )
 }
